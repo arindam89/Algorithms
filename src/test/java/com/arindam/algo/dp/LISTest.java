184422cda674d3340414977d81a5f519ab2b9559
Arindam Paul <arindampaul1989@gmail.com>
2014-09-10 12:05:47 +0530

    Added one more non-trivial test case

--- src/test/java/com/arindam/algo/dp/LISTest.java
+++ src/test/java/com/arindam/algo/dp/LISTest.java
@@ -29,6 +29,8 @@ public class LISTest {
         int[] B = {9, 8, 7, 6, 5, 4, 3, 2, 1};
         int[] C = {50, 2, 45, 3, 5, 44, 1, 8, 40, 12, 37, 15};
         int[] D = {2, 3, 5, 8, 12, 37};
+        int[] E = {3, 2, 6, 4, 5, 1};
+        int[] R = {3, 4, 5};
 
         int[] actual = seq.solve(A);
         assertTrue(Arrays.equals(A, actual));
@@ -37,5 +39,8 @@ public class LISTest {
 
         actual = seq.solve(C);
         assertTrue(Arrays.equals(D, actual));
+
+        actual = seq.solve(E);
+        assertArrayEquals(R,actual);
     }
 }
